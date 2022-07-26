cc84f381acbc2196704e81127aee8437b65962d4
tinker bell <43089867+tinkerrr@users.noreply.github.com>
2022-07-26 11:48:30 +0530

    Update csv_to_parque_gluejob.py

--- Week 3/csv_to_parque_gluejob.py
+++ Week 3/csv_to_parque_gluejob.py
@@ -6,15 +6,18 @@ import boto3
 from awsglue.utils import getResolvedOptions
 
 
+
 argProvidedByCaller = ['InputBucket', 'FileNameWithoutFiletype', 'OutputBucket']
 args = getResolvedOptions(sys.argv, argProvidedByCaller)
 s3 = boto3.resource('s3')
 
-byte_like_obj = s3.Object(args['InputBucket'], args['FileNameWithoutFiletype']+'.csv').get()['Body'].read()
-df = pd.read_csv(BytesIO(byte_like_obj))
+streaming_body = s3.Object(args['InputBucket'], args['FileNameWithoutFiletype']+'.csv').get()['Body']
+df = pd.read_csv(streaming_body)
 
 buffer = BytesIO() #_io.BytesIO object
 df.to_parquet(buffer, index=False)
 output_file_name = args['FileNameWithoutFiletype'] + '/' + 'data.parquet'
-s3.Object(args['OutputBucket'], output_file_name).put(Body=buffer.getvalue())
+s3.Object(args['OutputBucket'], output_file_name).put(Body=buffer.read())
 
+
+    
