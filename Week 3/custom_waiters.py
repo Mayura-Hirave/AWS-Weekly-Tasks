287cda694e2f67f68342be2e0b5d31f8ddf11416
Mayura Hirave <43089867+Mayura-Hirave@users.noreply.github.com>
2022-11-08 00:59:14 +0530

    Update custom_waiters.py

--- Week 3/custom_waiters.py
+++ Week 3/custom_waiters.py
@@ -11,34 +11,36 @@ class Waiter:
     def job_run_completed(Client, JobName, RunId, max_attempts=3, delay=50):
         num_attempts = 0
         while True:
+            time.sleep(delay)
             response = Client.get_job_run(JobName=JobName, RunId=RunId)
             if response['JobRun']['JobRunState'] in ('SUCCEEDED', 'FAILED'):
                 return response['JobRun']
             num_attempts += 1
             if num_attempts == max_attempts:
                 raise Exception("Max attempts exceeded. Current State: " + response['JobRun']['JobRunState'])
-            time.sleep(delay)
+            
 
     @staticmethod
     def crawler_run_completed(Client, CrawlerName, max_attempts=3, delay=60):
         num_attempts = 0
         while True:
+            time.sleep(delay)
             crawler = Client.get_crawler(Name=CrawlerName)['Crawler']
             if crawler['State'] == 'READY':
-                return crawler['LastCrawl'] 
+                return crawler['LastCrawl']
             num_attempts += 1
             if num_attempts == max_attempts:
                 raise Exception("Max attempts exceeded. Current State: " + crawler['State'])
-            time.sleep(delay)
 
     @staticmethod
     def query_run_completed(Client, QueryExecutionID, max_attempts=3, delay=5):
         num_attempts = 0
         while True:
+            time.sleep(delay)
             status = Client.get_query_execution(QueryExecutionId=QueryExecutionID)['QueryExecution']['Status']
             if status['State'] in ('SUCCEEDED', 'FAILED'):
                 return status
             num_attempts += 1
             if num_attempts == max_attempts:
                 raise Exception("Max attempts exceeded. Current State: " + status['State'])
-            time.sleep(delay)
+
