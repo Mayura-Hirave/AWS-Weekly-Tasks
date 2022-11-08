fbf2052b28389bd0cbe1ba01a77f85aa05119344
Mayura Hirave <43089867+Mayura-Hirave@users.noreply.github.com>
2022-11-08 11:47:23 +0530

    Update deleteS3ObjsBasedOnMetadataTags2.py

--- Week 2/deleteS3ObjsBasedOnMetadataTags2.py
+++ Week 2/deleteS3ObjsBasedOnMetadataTags2.py
@@ -1,150 +1,156 @@
 import boto3
-from aws_access_key import aws_access_key_ID,aws_secret_access_KEY
+from aws_access_key import aws_access_key_ID, aws_secret_access_KEY
 
 
 class CustomError(Exception):
     def __init__(self, description):
-        self.description=description
+        self.description = description
+
     def __str__(self):  # overriding print()
         return self.description
-        
+
+
 class Utility:
     @staticmethod
-    def compareMetadata(dict1, dict2):
-        for key,val in dict1.items():
-            if key not in dict2 or dict2[key]!=val:
+    def compare_metadata(dict1, dict2):
+        for key, val in dict1.items():
+            if key not in dict2 or dict2[key] != val:
                 return False
         return True
 
     # list1 = [{'Key': 'tag1', 'Value': 'tag1Val1'}, {'Key': 'tag2', 'Value': 'tag2Val1'}]
     @staticmethod
-    def compareTags( list1, list2):
+    def compare_tags(list1, list2):
         for dict1 in list1:
             if dict1 not in list2:
                 return False
         return True
 
     @staticmethod
-    def convertDictToString(tags):
+    def convert_dict_to_str(tags):
         return "&".join([k + "=" + v for k, v in tags.items()])
 
     @staticmethod
-    def takeDictInput(itemsCount):
-        dict1=dict()
-        for i in range(itemsCount):
+    def take_dict_input(items_cnt):
+        dict1 = dict()
+        for i in range(items_cnt):
             key_val = input("Enter in format => Key:Value  -> ").split(":")
             dict1[key_val[0]] = key_val[1]
         return dict1
 
     @staticmethod
-    def convertDictToTagset(dict1):
-        list1=[]
-        for key,val in dict1.items():
-            dict2=dict()
-            dict2['Key']=key
-            dict2['Value']=val
+    def convert_dict_to_tagset(dict1):
+        list1 = []
+        for key, val in dict1.items():
+            dict2 = dict()
+            dict2['Key'] = key
+            dict2['Value'] = val
             list1.append(dict2)
         return list1
 
-class s3bucket:
+
+class S3bucket:
     session = boto3.Session(aws_access_key_id=aws_access_key_ID, aws_secret_access_key=aws_secret_access_KEY,
                             region_name="ap-south-1")
     s3 = session.resource("s3")
-    s3Client = session.client("s3")  # created only for accessing object's tags - get_object_tagging()
-    def __init__(self,bucketName):
+    s3_client = session.client("s3")  # created only for accessing object's tags - get_object_tagging()
+
+    def __init__(self, bucket_name):
         try:
-            self.bucket = s3bucket.s3.Bucket(bucketName)
-            if bucketName not in [bucket.name for bucket in s3bucket.s3.buckets.all()]: # checking if bucket already exists
-                self.bucket.create(CreateBucketConfiguration={'LocationConstraint': 'ap-south-1' })
+            self.bucket = S3bucket.s3.Bucket(bucket_name)
+            if bucket_name not in [bucket.name for bucket in
+                                   S3bucket.s3.buckets.all()]:  # checking if bucket already exists
+                self.bucket.create(CreateBucketConfiguration={'LocationConstraint': 'ap-south-1'})
         except Exception as e:
-            raise CustomError("Error in s3bucket.__init__(): "+str(e))
-    def showObjects(self):
-        cntOfObjs=0
+            raise CustomError("Error in S3bucket.__init__(): " + str(e))
+
+    def show_objects(self):
+        obj_cnt = 0
         try:
             print("Object Key \t Metadata \t Tags")
-            for objectSummary in self.bucket.objects.all():
-                object = self.bucket.Object(objectSummary.key)
-                objTags = s3bucket.s3Client.get_object_tagging(Bucket=self.bucket.name, Key=object.key)['TagSet']
-                print(object.key,"\t",object.metadata,"\t",objTags)
-                cntOfObjs+=1
-            print("Total Objects: ",cntOfObjs)
+            for obj_summary in self.bucket.objects.all():
+                object = self.bucket.Object(obj_summary.key)
+                obj_tags = S3bucket.s3_client.get_object_tagging(Bucket=self.bucket.name, Key=object.key)['TagSet']
+                print(object.key, "\t", object.metadata, "\t", obj_tags)
+                obj_cnt += 1
+            print("Total Objects: ", obj_cnt)
         except Exception as e:
-            raise CustomError("Error in s3bucket.__init__(): "+str(e))
+            raise CustomError("Error in S3bucket.__init__(): " + str(e))
 
-    def DeleteObjsBasedOnMetadataTags(self,filterMetadata, filterTags):
-        objectsToDelete = []
+    def delete_objects(self, filter_metadata, filter_tags):
+        objects_to_delete = []
         try:
-            for objectSummary in self.bucket.objects.all():
-                object=self.bucket.Object(objectSummary.key)
-                objTags = s3bucket.s3Client.get_object_tagging(Bucket=self.bucket.name,Key=object.key)['TagSet']
-                #print(object.metadata,objTags)
-                if Utility.compareMetadata(filterMetadata, object.metadata) and Utility.compareTags(filterTags,objTags):
-                    objectsToDelete.append({'Key':object.key,'VersionId':'null'})
-            response=None
-            #print(objectsToDelete) -deleting these matched objects
-            if objectsToDelete:
-                response = self.bucket.delete_objects(Delete={'Objects': objectsToDelete, 'Quiet': True})
-            print("Successfully deleted",len(objectsToDelete),"objects!")
+            for obj_summary in self.bucket.objects.all():
+                object = self.bucket.Object(obj_summary.key)
+                obj_tags = S3bucket.s3_client.get_object_tagging(Bucket=self.bucket.name, Key=object.key)['TagSet']
+                # print(object.metadata,obj_tags)
+                if Utility.compare_metadata(filter_metadata, object.metadata) and Utility.compare_tags(filter_tags,
+                                                                                                       obj_tags):
+                    objects_to_delete.append({'Key': object.key, 'VersionId': 'null'})
+            response = None
+            # print(objects_to_delete) -deleting these matched objects
+            if objects_to_delete:
+                response = self.bucket.delete_objects(Delete={'Objects': objects_to_delete, 'Quiet': True})
+            print("Successfully deleted", len(objects_to_delete), "objects!")
             return response
         except Exception as e:
-            raise CustomError("Error in s3bucket.DeleteObjsBasedOnMetadataTags(): "+str(e))
+            raise CustomError("Error in S3bucket.delete_objects(): " + str(e))
 
-    def uploadObj(self, objName,metadata,fileContent,tags):
+    def upload_object(self, obj_name, metadata, file_content, tags):
         try:
-            resultDict = self.bucket.put_object(Key=objName, Body=fileContent, Metadata=metadata, Tagging=tags)
-            return resultDict
+            response = self.bucket.put_object(Key=obj_name, Body=file_content, Metadata=metadata, Tagging=tags)
+            return response
         except Exception as e:
-            raise CustomError("Error in s3bucket.uploadObj(): " + str(e))
+            raise CustomError("Error in S3bucket.upload_object(): " + str(e))
+
 
 if __name__ == '__main__':
-    try:
-        while True:
-            bucketName= input("Enter Bucket Name: ")
-            #bucketName='my-new-s3bucket'
-            myBucket = s3bucket(bucketName)
+    while True:
+        try:
+            bucket_name = input("Enter Bucket Name: ")
+            # bucket_name='my-new-s3bucket'
+            my_bucket = S3bucket(bucket_name)
             while True:
-                option=input("\nSelect any option:\n1. Put objects\n2. Display Object details\n3. Delete objects\n4. Exit\n=> ")
-                if option=='1':
-                    noOfObjs=input("How many objects?")
-                    while not noOfObjs.isnumeric():
-                        noOfObjs = input("Kindly enter integer! => ")
+                option = input("\nSelect any option:\n1. Put objects\n2. Display Object details\n3. Delete objects\n4. Exit\n=> ")
+                if option == '1':
+                    obj_cnt = input("How many objects?")
+                    while not obj_cnt.isnumeric():
+                        obj_cnt = input("Kindly enter integer! => ")
 
-                    noOfObjs=int(noOfObjs)
-                    for i in range(noOfObjs):
-                        objName = input("Enter Key of the object: ")
-                        fileContent = ""
-                        path=input("Enter path of input file=> ")  #like    C:\Users\Mayura\Downloads\dummy data.txt
-                        with open(path, 'rb') as fileObj:
-                            fileContent=fileObj.read()
-                        metadataCount = int(input("Count of metadata: "))
-                        metadata = Utility.takeDictInput(metadataCount)
-                        tagsCount = int(input("Number of Tags: "))
-                        tags = Utility.takeDictInput(tagsCount)
-                        myBucket.uploadObj(objName,metadata,fileContent,Utility.convertDictToString(tags))
-                    print("Successfully uploaded",noOfObjs,"objects")
-                elif option=='2':
-                    myBucket.showObjects()
-                elif option=='3':
-                    metadataCount = int(input("Count of metadata: "))
-                    metadata = Utility.takeDictInput(metadataCount)
-                    tagsCount = int(input("Number of Tags: "))
-                    tagsSet= Utility.convertDictToTagset(Utility.takeDictInput(tagsCount))
-                    response = myBucket.DeleteObjsBasedOnMetadataTags(metadata, tagsSet)
+                    obj_cnt = int(obj_cnt)
+                    for i in range(obj_cnt):
+                        obj_name = input("Enter Key of the object: ")
+                        file_content = ""
+                        path = input("Enter path of input file=> ")  # like    C:\Users\Mayura\Downloads\dummy data.txt
+                        with open(path, 'rb') as file_obj:
+                            file_content = file_obj.read()
+                        metadata_cnt = int(input("Count of metadata: "))
+                        metadata = Utility.take_dict_input(metadata_cnt)
+                        tags_cnt = int(input("Number of Tags: "))
+                        tags = Utility.take_dict_input(tags_cnt)
+                        my_bucket.upload_object(obj_name, metadata, file_content, Utility.convert_dict_to_str(tags))
+                    print("Successfully uploaded", obj_cnt, "objects")
+                elif option == '2':
+                    my_bucket.show_objects()
+                elif option == '3':
+                    metadata_cnt = int(input("Count of metadata: "))
+                    metadata = Utility.take_dict_input(metadata_cnt)
+                    tags_cnt = int(input("Number of Tags: "))
+                    tags_set = Utility.convert_dict_to_tagset(Utility.take_dict_input(tags_cnt))
+                    response = my_bucket.delete_objects(metadata, tags_set)
                     if response and response['ResponseMetadata']['HTTPStatusCode'] != 200:
                         print("Encountered Error!")
-                elif option=='4':
+                elif option == '4':
                     break
                 else:
                     print("Invalid Option!")
-            option=input("\nOptions:\n1. Work on other bucket\n2. Exit\n=> ")
-            if option=="1":
-                continue
-            elif option=="2":
-                break
-            else:
-                print("Invalid Option!")
-                break
-    except CustomError as e:
-        print(e)
-    except Exception as e:
-        print("Error: ",e)
+        except Exception as e:
+            print("Error: ", e)
+        option = input("\nOptions:\n1. Work on another bucket\n2. Exit\n=> ")
+        if option == "1":
+            continue
+        elif option == "2":
+            break
+        else:
+            print("Invalid Option!")
+            break
