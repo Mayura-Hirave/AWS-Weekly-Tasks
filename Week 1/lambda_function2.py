aa234eef351181ef63fea6382c2368d1d6120ec1
Mayura Hirave <43089867+Mayura-Hirave@users.noreply.github.com>
2022-11-08 11:21:16 +0530

    Update lambda_function2.py

--- Week 1/lambda_function2.py
+++ Week 1/lambda_function2.py
@@ -2,13 +2,13 @@ import boto3
 import urllib.parse
 
 def lambda_handler(event, context):
-	srcBucketName = urllib.parse.unquote(event['Records'][0]['s3']['bucket']['name'])
-	srcBucketObjKey = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
+	src_bucket_name = urllib.parse.unquote(event['Records'][0]['s3']['bucket']['name'])
+	src_bucket_objkey = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
 	
-	destinationBucket = 'backup-s3buckett1'
-	copy_source = {'Bucket': srcBucketName, 'Key': srcBucketObjKey }
+	destination_bucket = 'backup-s3buckett1'
+	copy_source = {'Bucket': src_bucket_name, 'Key': src_bucket_objkey }
 	try:
 		s3 = boto3.client('s3')
-		s3.copy_object(Bucket=destinationBucket, Key=srcBucketObjKey, CopySource=copy_source)
+		s3.copy_object(Bucket=destination_bucket, Key=src_bucket_objkey, CopySource=copy_source)
 	except Exception as err:
 		print ("Error -"+str(err))
